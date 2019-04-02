654849bd678eeb9ea6eaa8edb593c780bdec064e
gregzuro <greg@zuro.net>
2019-04-02 10:10:28 -0700

    remove old functions from main (moved to package)

--- three-colors.go
+++ three-colors.go
@@ -148,54 +148,6 @@ func getFileByURL(url string) (int64, io.ReadCloser, error) {
 	return resp.ContentLength, resp.Body, nil
 }
 
-func countColorsFromImage(image image.Image) map[string]uint {
-
-	bounds := image.Bounds()
-
-	// count the colors
-	colorCounts := make(map[string]uint) // this can have ~4MM entries - colors that jpeg can have (https://www.hackerfactor.com/blog/index.php?/archives/250-Showing-JPEGs-True-Color.html)
-	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-		for x := bounds.Min.X; x < bounds.Max.X; x++ {
-			r, g, b, _ := image.At(x, y).RGBA()
-			c := string([]byte{byte(r >> 8), byte(g >> 8), byte(b >> 8)}) // I notice an occasional off=by-one-count compared with photoshop-created images on (at least) the Blue channel)
-			// this is likely related to: https://www.hackerfactor.com/blog/index.php?/archives/250-Showing-JPEGs-True-Color.html as well...
-			colorCounts[c]++
-		}
-	}
-
-	return colorCounts
-}
-
-// findTopThreeFromCounts finds three most common colors from a colorCounts-style map
-// 	are we likely to want some number other (greater) than three?
-// 	if so, then do this a different way
-func findTopThreeFromCounts(colorCounts map[string]uint) []string {
-
-	topThree := make([]string, 3)
-	// ifCount := 0
-	for c, n := range colorCounts {
-		// ifCount++
-		if n > colorCounts[topThree[2]] { // first check to see if this one is in the top three at all
-			// ifCount++
-			if n > colorCounts[topThree[0]] { // then start check from most common (first) down to least (third)
-				// this couples the least number of reassighments with the least number of conditionals...
-				topThree[2] = topThree[1]
-				topThree[1] = topThree[0]
-				topThree[0] = c
-			} else {
-				// ifCount++
-				if n > colorCounts[topThree[1]] {
-					topThree[2] = topThree[1]
-					topThree[1] = c
-				} else {
-					topThree[2] = c
-				}
-			}
-		}
-	}
-	return topThree
-}
-
 // printResults prints out the results in CSV style
 func printResults(fileURL string, topThree []string, colorCounts map[string]uint, duplicate bool) {
 
